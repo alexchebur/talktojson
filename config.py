ecffdf63653ddba6a88107967321385e4550600b
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-04-01 23:20:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 import os
 
-API_KEY = "sk-or-vv-a8d6e009e2bbe09474b0679fbba83b015ff1c4f255ed76f33b48ccb1632bdc32
+API_KEY = "sk-or-vv-a8d6e009e2bbe09474b0679fbba83b015ff1c4f255ed76f33b48ccb1632bdc32"
 API_URL = "https://api.vsegpt.ru/v1/chat/completions"
