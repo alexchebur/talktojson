3390aabd0c1f134c10477a4b75d936ee675aeca7
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-04-06 20:29:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -210,7 +210,7 @@ class DocumentAnalyzer:
             return
             
         try:
-            if uploaded_files.size == 0:
+            if len(uploaded_files) = 0:
                 st.warning("Файл пуст")
                 return
                 
