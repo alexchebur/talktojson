fa7673732d7ff9a4966a30106f79b0163b507f0e
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-04-10 17:19:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 import os
-API_KEY = os.getenv("VS_EGPT_API_KEY", "")
+API_KEY = "sk-or-vv-a8d6e009e2bbe09474b0679fbba83b015ff1c4f255ed76f33b48ccb1632bdc32"
 API_URL = "https://api.vsegpt.ru/v1/chat/completions"
