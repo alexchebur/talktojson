e73740ee67f5d421962576f98a44f3a1d4d00066
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-07-11 12:35:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 import os
 API_KEY = "sk-or-vv-a8d6e009e2bbe09474b0679fbba83b015ff1c4f255ed76f33b48ccb1632bdc32"
-API_URL = "https://api.vsegpt.ru/v1/chat/completions"
+API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite-preview-06-17:generateContent"
