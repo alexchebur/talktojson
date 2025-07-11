9695d9e2796d4fc4d15659d20f0f4f9fc00fec3d
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-07-11 16:31:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 import os
 API_KEY = "AIzaSyCGC2JB3BgfBMycbt4us1eq6D5exNOvKT8"
-API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite-preview-06-17"
+API_URL = "https://generativelanguage.googleapis.com/v1beta/"
 
