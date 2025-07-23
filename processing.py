9da06a1456e82e4a89c9d2b42247652b6dc1638c
alexchebur <93209745+alexchebur@users.noreply.github.com>
2025-07-23 16:58:43 +0500

    Update processing.py

--- processing.py
+++ processing.py
@@ -4,6 +4,7 @@ import numpy as np
 from difflib import SequenceMatcher
 from typing import List
 from utils import clean_keyword
+from rank_bm25 import BM25Okapi
 
 class DataProcessor:
     def __init__(self, index_builder):
